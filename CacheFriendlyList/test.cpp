9baa765201a651bce4ce4f7656123eb5124a2766
username <emailaddress@yahoo.com>
2021-11-18 03:41:51 +0300

    more tests

--- CacheFriendlyList/test.cpp
+++ CacheFriendlyList/test.cpp
@@ -3,17 +3,78 @@
 
 TEST (ListTests, InsertBackWithEmpty) {
     val_t array[List::DEFAULT_CAPACITY] = {};
-    for (int i = 0; i < List::DEFAULT_CAPACITY; ++i) {
+    for (size_t i = 0; i < List::DEFAULT_CAPACITY; ++i) {
         array[i] = i*i;
     }
 
     List list{};
-    for (int i = 0; i < List::DEFAULT_CAPACITY; ++i) {
+    for (size_t i = 0; i < List::DEFAULT_CAPACITY; ++i) {
         list.insert_back (array[i]);
     }
 
     auto lcurr = list.head(); 
-    for (int i = 0; i < List::DEFAULT_CAPACITY; ++i, lcurr = list.next (lcurr)) {
+    for (size_t i = 0; i < List::DEFAULT_CAPACITY; ++i, lcurr = list.next (lcurr)) {
+        ASSERT_EQ (list.get (lcurr), array[i]);
+    }
+}
+
+TEST (ListTests, InsertFrontWithEmpty) {
+    val_t array[List::DEFAULT_CAPACITY] = {};
+    for (size_t i = 0; i < List::DEFAULT_CAPACITY; ++i) {
+        array[i] = i*i;
+    }
+
+    List list{};
+    for (size_t i = 0; i < List::DEFAULT_CAPACITY; ++i) {
+        list.insert_front (array[i]);
+    }
+
+    auto lcurr = list.head(); 
+    for (size_t i = 0; i < List::DEFAULT_CAPACITY; ++i, lcurr = list.next (lcurr)) {
+        ASSERT_EQ (list.get (lcurr), array[List::DEFAULT_CAPACITY - i - 1]);
+    }
+}
+
+TEST (ListTests, InsertPrev) {
+    val_t array[List::DEFAULT_CAPACITY] = {};
+    for (size_t i = 0; i < List::DEFAULT_CAPACITY; ++i) {
+        array[i] = i*i;
+    }
+
+    List list{};
+    ref_t pos[List::DEFAULT_CAPACITY] = {};
+    for (size_t i = 1; i < List::DEFAULT_CAPACITY; i += 2) {
+        pos [i] = list.insert_back (array[i]);
+    }
+
+    for (size_t i = 0; i < List::DEFAULT_CAPACITY; i += 2 ) {
+        list.insert_before (pos[i+1], array[i]);
+    }
+
+    auto lcurr = list.head(); 
+    for (size_t i = 0; i < List::DEFAULT_CAPACITY; ++i, lcurr = list.next (lcurr)) {
+        ASSERT_EQ (list.get (lcurr), array[i]);
+    }
+}
+
+TEST (ListTests, InsertAfter) {
+    val_t array[List::DEFAULT_CAPACITY] = {};
+    for (size_t i = 0; i < List::DEFAULT_CAPACITY; ++i) {
+        array[i] = i*i;
+    }
+
+    List list{};
+    ref_t pos[List::DEFAULT_CAPACITY] = {};
+    for (size_t i = 0; i < List::DEFAULT_CAPACITY; i += 2) {
+        pos [i] = list.insert_back (array[i]);
+    }
+
+    for (size_t i = 1; i < List::DEFAULT_CAPACITY; i += 2 ) {
+        list.insert_after (pos[i-1], array[i]);
+    }
+
+    auto lcurr = list.head(); 
+    for (size_t i = 0; i < List::DEFAULT_CAPACITY; ++i, lcurr = list.next (lcurr)) {
         ASSERT_EQ (list.get (lcurr), array[i]);
     }
 }
